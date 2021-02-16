a2281567ece1f8b594563c1e10a75f995af4f2f2
Rebecca Bakels <rebeccabakels@gmail.com>
2021-02-16 17:05:41 -0500

    set up base API request

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
 const functions = require("firebase-functions");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/activities', (req, res) => {
+    //GET activities from firestore
+    //form the docs we get back into a json array
+    //optional: setup Caching
+    //send that json array with status 200
+    res.status(200).json([{ 
+        name: 'Client Work',
+        totalTime: 900,
+
+    }, {
+        name: 'Personal Project',
+        totalTime: 750
+    }])
+})
+
+exports.app = functions.https.onRequest(app)
