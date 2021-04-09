c6713a6353a51b4463e7930307aa9b75ae82049b
Rebecca Bakels <rebeccabakels@gmail.com>
2021-04-09 12:52:12 -0400

    formatted

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,21 @@
 const functions = require("firebase-functions");
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { getActivities, postActivity, patchActivity, deleteActivity} = require('./src/activities')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const {
+  getActivities,
+  postActivity,
+  patchActivity,
+  deleteActivity,
+} = require("./src/activities");
 
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
+app.get("/activities", getActivities);
+app.post("/activities", postActivity);
+app.patch("/activities/:activityId", patchActivity);
+app.delete("/activities/:activityId", deleteActivity);
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
-
-app.get('/activities', getActivities)
-app.post('/activities', postActivity)
-app.patch('/activities/:activityId', patchActivity)
-app.delete('/activities/:activityId', deleteActivity)
-
-exports.app = functions.https.onRequest(app)
+exports.app = functions.https.onRequest(app);
--- functions/src/activities/index.js
+++ functions/src/activities/index.js
@@ -1,85 +1,86 @@
-const admin = require('firebase-admin')
-const serviceAccount = require('../../credentials.json')
+const admin = require("firebase-admin");
+const serviceAccount = require("../../credentials.json");
 
-let db
+let db;
 
 function authDB() {
-    if (!admin.apps.length) {
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-        })
-    }
-   db = admin.firestore()
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  }
+  db = admin.firestore();
 }
 
 exports.getActivities = (req, res) => {
-    authDB()
-    db.collection('activities').get()   //.where('userId', '==', userId)
-    .then(collection => {
-        const activityResults = collection.docs.map(activity => {
-            let newActivity = activity.data()
-            newActivity.id =activity.id
-            return newActivity
-        })
-        res.status(200).json(activityResults)
+  authDB();
+  db.collection("activities")
+    .get() //.where('userId', '==', userId)
+    .then((collection) => {
+      const activityResults = collection.docs.map((activity) => {
+        let newActivity = activity.data();
+        newActivity.id = activity.id;
+        return newActivity;
+      });
+      res.status(200).json(activityResults);
     })
-    .catch(err => res.status(500).send('error getting activities:' + err))
-}
+    .catch((err) => res.status(500).send("error getting activities:" + err));
+};
 
 exports.postActivity = (req, res) => {
-    if(!req.body || !req.body.name || !req.body.userId){
-        res.status(401).send('Missing new Activity data')
-    }
-    authDB()
-    const newActivityRequest = req.body
-    let now = admin.firestore.FieldValue.serverTimestamp()
-    let newActivity = {
-        name: req.body.name,
-        totalDuration: 0,
-        userid: req.body.userId,
-        logs: [],
-        created: now,
-        updated: now,
-    }
-    db.collection('activities').add(newActivity)
+  if (!req.body || !req.body.name || !req.body.userId) {
+    res.status(401).send("Missing new Activity data");
+  }
+  authDB();
+  const newActivityRequest = req.body;
+  let now = admin.firestore.FieldValue.serverTimestamp();
+  let newActivity = {
+    name: req.body.name,
+    totalDuration: 0,
+    userid: req.body.userId,
+    logs: [],
+    created: now,
+    updated: now,
+  };
+  db.collection("activities")
+    .add(newActivity)
     .then(() => {
-        this.getActivities(req, res)
+      this.getActivities(req, res);
     })
-    .catch(err => res.status(500).send('error creating activity:' + err))
-}
+    .catch((err) => res.status(500).send("error creating activity:" + err));
+};
 
 exports.patchActivity = (req, res) => {
-    if(!req.body || !req.body.duration || !req.params.activityId ){ //|| !req.params.userId
-        res,status(400).send('invalid request')
-    }
-    authDB()
-    let now = admin.firestore.FieldValue.serverTimestamp()
-    db.collection('activities').doc(req.params.activityId).update({
-        updated: now,
-        logs: admin.firestore.FieldValue.arrayUnion(req.body),
-        totalDuration: admin.firestore.FieldValue.increment(Number(req.body.duration))
-    })
-    .then(() => {
-        this.getActivities(req, res)
-    })
-    .catch(err => res.status(500).send('error updating activity:' + err))
-
-}
-exports.deleteActivity = (req, res) => {
-    if(!req.params.activityId){
-        res.status(400).send('Invalid Request')  
-        }
-    authDB()
-    db.collection('activities').doc(req.params.activityId).delete()
-    .then(() => {
-      res.status(200).json({
-        status: "success",
-        message: "Activity DELETED",
-        statusCode: 200,
-      })
-    .then(() => {
-        this.getActivities(req, res)
-    })
-    })
-    .catch(err => res.status(500).send('delete failed:', err))
+  if (!req.body || !req.body.duration || !req.params.activityId) {
+    //|| !req.params.userId
+    res, status(400).send("invalid request");
   }
+  authDB();
+  let now = admin.firestore.FieldValue.serverTimestamp();
+  db.collection("activities")
+    .doc(req.params.activityId)
+    .update({
+      updated: now,
+      logs: admin.firestore.FieldValue.arrayUnion(req.body),
+      totalDuration: admin.firestore.FieldValue.increment(
+        Number(req.body.duration)
+      ),
+    })
+    .then(() => {
+      this.getActivities(req, res);
+    })
+    .catch((err) => res.status(500).send("error updating activity:" + err));
+};
+exports.deleteActivity = (req, res) => {
+  if (!req.params.activityId) {
+    res.status(400).send("Invalid Request");
+  }
+  authDB();
+  db.collection("activities")
+    .doc(req.params.activityId)
+    .delete()
+    .then(() => {
+      this.getActivities(req, res);
+    })
+    .catch((err) => res.status(500).send("delete failed:", err));
+};
